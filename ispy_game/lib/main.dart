329af8f893621c4561df6bcdba0529b9848a0769
Alexandria <alexandriacade0@gmail.com>
2022-09-29 19:09:07 -0500

    Initialize all screen pages
    Creating files for all screen pages, refactored Flutter generated title, MyApp, to EyeSpy

--- ispy_game/lib/main.dart
+++ ispy_game/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const EyeSpy());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EyeSpy extends StatelessWidget {
+  const EyeSpy({super.key});
 
   // This widget is the root of your application.
   @override
