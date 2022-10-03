f5949f92cf61c79c879093168ee0a8f5d4ee1433
Garrett <colliergc@hendrix.edu>
2022-10-02 21:37:57 -0500

    Add ImagePicker package and setup pages for image select and navigation

--- ispy_game/lib/home_screen.dart
+++ ispy_game/lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ispy_game/main.dart';
+import 'package:ispy_game/image_selection_screen.dart';
+import 'package:ispy_game/send_image_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,20 +10,92 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // Text input window when guessing or responding
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    print("Loading Dialog");
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Guess'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            // position
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextField(
+                onChanged: (value) {
+                  setState(
+                    () {
+                      value = value;
+                    },
+                  );
+                },
+                //controller: _spyInputController,
+                decoration: const InputDecoration(hintText: "Content"),
+              ),
+              ElevatedButton(
+                key: const Key("OKButton"),
+                child: const Text('OK'),
+                onPressed: () {
+                  setState(
+                    () {},
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Screen"),
-      ),
-      body: Column(children:[Row(
-        children: [Expanded(child: TextButton(onPressed: (){}, child: const Text("Share an Image"))), 
-        Expanded(child: TextButton(onPressed: (){}, child: const Text("Resond to a Guess"))),
-        Expanded(child: TextButton(onPressed: (){}, child: const Text("Respond to an Image")))
-        ]),
-        Row(children:[Center(child: Text("Game Chat"))])]
+        appBar: AppBar(
+          title: const Text("Home Screen"),
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const SelectImageScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text("Share an Image"),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Respond to a Guess"),
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text("Respond to an Image"),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: const [
+                Center(
+                  child: Text("Game Chat"),
+                )
+              ],
+            )
+          ],
         )
         //Game chat,
-    );
+        );
   }
 }
--- ispy_game/lib/image_selection_screen.dart
+++ ispy_game/lib/image_selection_screen.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:ispy_game/main.dart';
+import 'package:ispy_game/send_image_screen.dart';
+
+enum ImageSourceType { gallery, camera }
 
 class SelectImageScreen extends StatelessWidget {
   const SelectImageScreen({super.key});
 
+  void _handleURLButtonPress(BuildContext context, var type) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => SendImageScreen(type)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Select Image Source')),
-      body: Column(children: [TextButton(onPressed: () {}, child: const Text("Upload Image")), 
-      TextButton(onPressed: () {}, child: const Text("Take a Picture"))]
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Profile Picture for Blog"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                key: const Key("GalleryButton"),
+                color: Colors.blue,
+                child: const Text(
+                  "Pick an Image!",
+                  style: TextStyle(
+                      color: Colors.white70, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  _handleURLButtonPress(context, ImageSourceType.gallery);
+                },
+              ),
+              MaterialButton(
+                key: const Key("CameraButton"),
+                color: Colors.blue,
+                child: const Text(
+                  "Take a Picture!",
+                  style: TextStyle(
+                      color: Colors.white70, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  _handleURLButtonPress(context, ImageSourceType.camera);
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
