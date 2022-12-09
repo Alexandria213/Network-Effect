01c3ec67cbd0d83320205388c63f3632cf01f6ad
Gaëlle Agahozo <agahozogaelle4@gmail.com>
2022-12-09 02:14:12 -0600

    added a bottom navigation bar to make navigation easy

---
+++ ispy_game/lib/nav.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:ispy_game/home_screen.dart';
+import 'package:ispy_game/image_selection_screen.dart';
+import 'package:ispy_game/takepictureScreen.dart';
+import 'package:ispy_game/send_image_screen.dart';
+import 'package:ispy_game/scoring.dart';
+import 'package:ispy_game/server.dart';
+import 'package:ispy_game/game_chat.dart';
+import 'package:ispy_game/friends.dart';
+import 'package:ispy_game/main.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Weather App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const NavigationScaffold());
+  }
+}
+
+class NavigationScaffold extends StatefulWidget {
+  const NavigationScaffold({super.key});
+
+  @override
+  State<NavigationScaffold> createState() => _NavigationScaffoldState();
+}
+
+class _NavigationScaffoldState extends State<NavigationScaffold> {
+  int screenIndex = 0;
+  final screens = [
+    Home(),
+    Scoring(),
+  ];
+
+  void updateScreenIndex(int newScreenIndex) {
+    setState(() {
+      screenIndex = newScreenIndex;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[screenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: screenIndex,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.score), label: "Score"),
+        ],
+        onTap: updateScreenIndex,
+        selectedItemColor: Color.fromARGB(255, 140, 14, 14),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
